e7ddc47df274f405eda2ffceee2cb82491bb4011
dubious-soup <impoliashenko@gmail.com>
2026-02-28 05:12:59 +0000

    Added FSA and FST with basic example which showcases translating strings

---
+++ automata.py
@@ -0,0 +1,126 @@
+from itertools import chain
+from collections import deque
+
+#Finite State Automata
+class FSA:
+    def __init__(self, states, alphabet, transitions, initial_states, final_states):
+        self.states = states
+        self.alphabet = alphabet
+        self.transitions = transitions
+        self.initial_states = initial_states
+        self.final_states = final_states
+
+    def __str__(self):
+        adjacency_table = ""
+        for state, transition in self.transitions.items():
+            for symbol, new_states in transition.items():
+                adjacency_table += f"\n{state} -({symbol})> {new_states}"
+        return adjacency_table
+    
+    def validate_path(self, string, state):
+        if len(string) == 0:
+            if state in self.final_states:
+                return True
+            else:
+                return False
+        
+        if (string[0] in self.transitions[state]):
+            for new_state in self.transitions[state][string[0]]:
+                if self.validate_path(string[1:],new_state):
+                    return True
+        else:
+            return False
+
+
+    def accept(self, string):
+        for initial_state in self.initial_states:
+            if self.validate_path(string, initial_state):
+                return True
+        return False
+
+#Finite State Transducer
+class FST:
+
+    def __init__(self, states, input_alphabet, output_alphabet, transitions, initial_states, final_states):
+        self.states = states
+        self.input_alphabet = input_alphabet
+        self.output_alphabet = output_alphabet
+        self.transitions = transitions
+        self.initial_states = initial_states
+        self.final_states = final_states
+
+    def __str__(self):
+        adjacency_table = ""
+        for state, transition in self.transitions.items():
+            for symbol, new_states in transition.items():
+                for new_state in new_states:
+                    adjacency_table += f"\n{state} -({symbol}|{new_state[0]})> {new_state[1]}"
+        return adjacency_table
+    
+    def translate_path(self, string, state):
+        e_new_translated_ends = set()
+        new_translated_ends = set()
+
+        if ('e' in self.transitions[state]):
+            for new_state in self.transitions[state]['e']:
+                translated_ends = self.translate_path(string,new_state[1])
+                for translated_end in translated_ends:
+                    translated_end = deque(list(translated_end))
+                    translated_end.appendleft(new_state[0])
+                    e_new_translated_ends.add(tuple(translated_end))
+        else:
+            if len(string) == 0:
+                if state in self.final_states:
+                    print("accepted string!!")
+                    return {('e',)}
+                else:
+                    return set()
+        
+        if len(string) == 0:
+            return e_new_translated_ends
+
+        if (string[0] in self.transitions[state]):
+            for new_state in self.transitions[state][string[0]]:
+                translated_ends = self.translate_path(string[1:],new_state[1])
+                for translated_end in translated_ends:
+                    translated_end = deque(list(translated_end))
+                    translated_end.appendleft(new_state[0])
+                    new_translated_ends.add(tuple(translated_end))
+        print(e_new_translated_ends|new_translated_ends)
+        return e_new_translated_ends|new_translated_ends
+
+    def validate_path(self, string, state):
+        if len(string) == 0:
+            if state in self.final_states:
+                return True
+            else:
+                return False
+        
+        if (string[0] in self.transitions[state]):
+            for new_state in self.transitions[state][string[0]]:
+                if self.validate_path(string[1:],new_state[1]):
+                    return True
+        else:
+            return False
+
+    @staticmethod
+    def remove_empty_symbols(messy_set, is_str):
+        cleaned_set = set()
+        for string in messy_set:
+            string = [symbol for symbol in list(string) if symbol != 'e']
+            if is_str:
+                string = "".join(string)
+            else:
+                string = tuple(string)
+            cleaned_set.add(string)
+        return cleaned_set
+
+    def accept(self, string, is_str = False):
+        if is_str:
+            string = list(string)
+        translations = set()
+        for initial_state in self.initial_states:
+            translations = translations|self.translate_path(string, initial_state)
+        return self.remove_empty_symbols(translations, is_str)
+
+        
--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print(hi)
+from automata import FSA
+from automata import FST
+
+states = {'s0','s1'}
+alphabet = {'a','b'}
+transitions = {'s0':{'a':{'s0'},'b':{'s1'}},
+                's1':{'a':{'s1'},'b':{'s1'}}}
+initial_states = {'s0'}
+final_states = {'s1'}
+
+my_automata = FSA(states,alphabet,transitions,initial_states,final_states)
+
+states = {'s0','s1','s2','s3','s4','s5'}
+input_alphabet = {'a'}
+output_alphabet = {'a','1','2','3'}
+transitions = {'s0':{'a':{('1','s1'),('2','s2'),('3','s3')}},
+                's1':{'e':{('a','s5')}},
+                's2':{'a':{('a','s5')}},
+                's3':{'a':{('a','s4')}},
+                's4':{'a':{('e','s5')}},
+                's5':{}}
+initial_states = {'s0'}
+final_states = {'s5'}
+
+bounded_a_counter = FST(states,input_alphabet,output_alphabet,transitions,initial_states,final_states)
+
+print(bounded_a_counter)
+print(bounded_a_counter.accept('a', is_str = True))
